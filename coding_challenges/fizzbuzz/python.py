51f24d13c0b3f37bd9358e83c254061fd5588e5d
abiola <abiola.arobieke@gmail.com>
2024-09-12 07:06:40 -0700

    Modidy python.py file

--- coding_challenges/fizzbuzz/python.py
+++ coding_challenges/fizzbuzz/python.py
@@ -1,15 +1,8 @@
 def fizzbuzz():
-    result = []
-    for num in range(1, 101):
-        if num % 3 == 0 and num % 5 == 0:
-            result.append("FizzBuzz")
-        elif num % 5 == 0:
-            result.append("Buzz")
-        elif num % 3 == 0:
-            result.append("Fizz")
-        else:
-            result.append(num)
-    return result
-
-
+    return [
+        "FizzBuzz" if num % 3 == 0 and num % 5 == 0 else
+        "Buzz" if num % 5 == 0 else
+        "Fizz" if num % 3 == 0 else num
+        for num in range(1, 101)
+    ]
 print(fizzbuzz())
