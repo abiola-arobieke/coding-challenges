6ffb327783f7022971fb205f0bc24484fb15a238
abiola <abiola.arobieke@gmail.com>
2024-09-13 14:13:33 -0700

    Update anagram/python.js file

--- coding_challenges/anagram/python.py
+++ coding_challenges/anagram/python.py
@@ -1,9 +1,17 @@
 def anagram(str1, str2):
-    first_word = ''.join(sorted(''.join(letter for letter in str1 if letter.isalnum()).lower()))
-    second_word = ''.join(sorted(''.join(letter for letter in str2 if letter.isalnum()).lower()))
+    def clean_string(str):
+        return ''.join(sorted(letter.lower() for letter in str if letter.isalnum()))
 
-    return first_word == second_word
+    return clean_string(str1) == clean_string(str2)
 
 
 print(anagram("frienD", "Finder"))
 print(anagram("Horse", "Shoe"))
+
+
+
+# def anagram(str1, str2):
+#     first_word = ''.join(sorted(''.join(letter for letter in str1 if letter.isalnum()).lower()))
+#     second_word = ''.join(sorted(''.join(letter for letter in str2 if letter.isalnum()).lower()))
+
+#     return first_word == second_word
