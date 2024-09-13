729dadcf4355adad7f791df3b344c226da4fef8f
abiola <abiola.arobieke@gmail.com>
2024-09-13 14:14:06 -0700

    Update anagran/ruby.rb file

--- coding_challenges/anagram/ruby.rb
+++ coding_challenges/anagram/ruby.rb
@@ -1,9 +1,15 @@
 def anagram(str1, str2)
-  first_word = str1.downcase().delete('^a-zA-Z0-9').chars.sort.join
-  second_word = str2.downcase().delete('^a-zA-Z0-9').chars.sort.join
-
-  first_word == second_word
+  clean_string = ->(str) { str.downcase.delete('^a-z0-9').chars.sort.join }
+  
+  clean_string.call(str1) == clean_string.call(str2)
 end
 
 p anagram("frienD", "Finder")
 p anagram("Horse", "Shoe")
+
+# def anagram(str1, str2)
+#   first_word = str1.downcase().delete('^a-zA-Z0-9').chars.sort.join
+#   second_word = str2.downcase().delete('^a-zA-Z0-9').chars.sort.join
+
+#   first_word == second_word
+# end
