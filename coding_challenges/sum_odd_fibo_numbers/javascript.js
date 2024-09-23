d92936e33f68b8cf5b55fd56ab19e07b31541d06
abiola <abiola.arobieke@gmail.com>
2024-09-23 04:43:12 -0700

    Update sumFiboNum method

--- coding_challenges/sum_odd_fibo_numbers/javascript.js
+++ coding_challenges/sum_odd_fibo_numbers/javascript.js
@@ -1,16 +1,13 @@
 const sumFiboNum = (num) => {
-    i = 1;
-    j = 0;
-    odd_sum = 0;
+   let i = 1, j = 0, odd_sum = 0;
 
     while (i < num) {
         if (i % 2 !== 0) {
             odd_sum += i
         }
-        i += j;
-        j = i - j;
+        [i, j] = [i + j, i];
     }
     return odd_sum;
 }
 
-console.log(sum_odd_fibo_num(20))
+console.log(sumFiboNum(10))
